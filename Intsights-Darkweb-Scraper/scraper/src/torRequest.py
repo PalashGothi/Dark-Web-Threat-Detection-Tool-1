d769efad741ca9de7df7fbcbb8abe7dc98604c69
DiviyamPathak <patdiviyam@gmail.com>
2025-01-23 05:55:37 +0530

    working

--- Intsights-Darkweb-Scraper/scraper/src/torRequest.py
+++ Intsights-Darkweb-Scraper/scraper/src/torRequest.py
@@ -4,7 +4,7 @@ import requests
 # Gets a response from a website in Tor by proxying
 def get_tor_content(url: str):
     session = requests.session()
-    session.proxies["http"] = "socks5h://tor:9050"
-    session.proxies["https"] = "socks5h://tor:9050"
+    session.proxies["http"] = "socks5h://tor:9057"
+    session.proxies["https"] = "socks5h://tor:9057"
     response = session.get(url, timeout=60)
     return response.content
